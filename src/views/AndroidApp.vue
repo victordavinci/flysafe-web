5a0d3384acd4860b07483e9ebaec9fb1691fbb17
victordavinci <victordavinci@users.noreply.github.com>
2019-08-15 15:31:43 -0300

    Actualizar

--- src/views/AndroidApp.vue
+++ src/views/AndroidApp.vue
@@ -8,7 +8,7 @@
         <p><a href="/app/flysafe-1.1.apk" class="download"><font-awesome-icon :icon="['fas', 'download']" size="lg" /> {{ $t("message.download") }} flysafe-1.1.apk</a> (SHA256: d33441b8bef5884f0a80e767db08d3761eb10ff541140ceedb9b0f9577c18b8c)</p>
         <p><a href="/app/flysafe-1.2.apk" class="download"><font-awesome-icon :icon="['fas', 'download']" size="lg" /> {{ $t("message.download") }} flysafe-1.2.apk</a> (SHA256: 2f51d59edeee4b9a67e75f0d58a6e14f2340aedbd8dcda4909586a305a4ae141)</p>
         <p><a href="/app/flysafe-1.3.apk" class="download"><font-awesome-icon :icon="['fas', 'download']" size="lg" /> {{ $t("message.download") }} flysafe-1.3.apk</a> (SHA256: 1a0467403b34ccfd065aa69b192b8f9a95315e56ab6d72d2f61708004d732763)</p>
-        <p><a href="/app/flysafe-1.4.apk" class="download"><font-awesome-icon :icon="['fas', 'download']" size="lg" /> {{ $t("message.download") }} flysafe-1.4.apk</a> (SHA256: ec54d3d082fbd4a3f59f97fb8039b7638992b5f53637e438d1f7aa97ff037e3f)</p>
+        <p><a href="/app/flysafe-1.4.apk" class="download"><font-awesome-icon :icon="['fas', 'download']" size="lg" /> {{ $t("message.download") }} flysafe-1.4.apk</a> (SHA256: 96ce8a7b86cee25c6bb06a579991807d26207660da456e9c51b6048c77a47fd1)</p>
     </div>
 </template>
 
---
+++ src/views/Options.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id="options">
+        <ul>
+            <li><router-link to="/datos-personales">{{ $t("message.account_data") }}</router-link></li>
+            <li>
+                <a href="#" @click.prevent="toggleLang">
+                    <span v-if="lang=='en'">Cambiar a Español</span>
+                    <span v-if="lang=='es'">Switch to English</span>
+                </a>
+            </li>
+            <li><router-link to="/documentacion">{{ $t("message.help") }}</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Options",
+    computed: {
+        lang: function() {
+            return this.$store.state.i18n.locale;
+        },
+    },
+    methods: {
+        toggleLang: function() {
+            var lang = this.$store.state.i18n.locale;
+            this.$store.state.i18n.locale = lang == "en" ? "es" : "en";
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    padding-left: 0;
+}
+li {
+    padding: 0.25em 0;
+    text-align: center;
+}
+li a {
+    color: #222;
+    text-decoration: none;
+    display: inline-block;
+    width: 15em;
+    border: 1px solid #f5f5f5;
+    border-radius: 0.25em;
+    padding: 0.5em 0;
+    background: #f5f5f5;
+    background: linear-gradient(#f5f5f5, #e5e5e5);
+    transition: border-color 0.5s;
+}
+li a:hover {
+    border: 1px solid #d5d5d5;
+}
+li a:active {
+    background: linear-gradient(#e5e5e5, #f5f5f5);
+}
+</style>
